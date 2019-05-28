c5d7e565b41fad17f5a08157ba11542c26765076
Yeh, Jamie <jamie.yeh@disney.com>
2019-05-28 13:15:37 -0700

    passing data

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
 
     }
     
@@ -19,4 +25,27 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
 
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
+    
 }
---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Yeh, Jamie on 5/28/19.
+//  Copyright © 2019 JW. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}

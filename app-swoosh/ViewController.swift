5723f52370e1e2dc5e4d8ab0e69cede47869f950
Yeh, Jamie <jamie.yeh@disney.com>
2019-05-28 10:44:19 -0700

    segues

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
     
@@ -17,6 +16,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
     
 }
 
